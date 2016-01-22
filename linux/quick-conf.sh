b1dc3a1ff4780e8034754c426a0a8765dbe9cef0
André Martins <andre@andre.infomaniak.ch>
2016-01-22 16:52:52 +0100

    [WIP] Ajout de packages

---
+++ linux/quick-conf.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#http://askubuntu.com/questions/519/how-do-i-write-an-application-install-shell-script
+cd
+apt-get update
+apt-get install htop -y
+apt-get install iftop -y
+apt-get install git -y
+
+#Add repo php7
+add-apt-repository ppa:ondrej/php -y
+apt-get update
+apt-get install php7.0 -y
+apt-get install php7.0-mysql -y
+php -r "readfile('https://getcomposer.org/installer');" | php
+mv composer.phar /usr/local/bin/composer
+
+apt-get install transmission -y
+wget -P $HOME/atom/ https://atom.io/download/deb && dpkg --install $HOME/atom/deb
