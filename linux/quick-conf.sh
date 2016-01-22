504541fb39b6bee61b11076a5669386982ef3391
André Martins <andre@andre.infomaniak.ch>
2016-01-22 17:14:51 +0100

    [WIP] ajout packages

--- linux/quick-conf.sh
+++ linux/quick-conf.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 #http://askubuntu.com/questions/519/how-do-i-write-an-application-install-shell-script
-cd
+cd $HOME
 apt-get update
 apt-get install htop -y
 apt-get install iftop -y
 apt-get install git -y
+apt-get install mysql-server -y
 
 #Add repo php7
 add-apt-repository ppa:ondrej/php -y
@@ -13,6 +14,7 @@ apt-get install php7.0 -y
 apt-get install php7.0-mysql -y
 php -r "readfile('https://getcomposer.org/installer');" | php
 mv composer.phar /usr/local/bin/composer
+
 
 apt-get install transmission -y
 wget -P $HOME/atom/ https://atom.io/download/deb && dpkg --install $HOME/atom/deb
